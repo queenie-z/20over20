a0668e8192e33bc9ca2182b0703a5294d309c03b
Queenie <qwertyuigid@gmail.com>
2023-10-27 22:59:54 -0600

    Adding first project skeleton with pseudocode.

---
+++ 1Average.cpp
@@ -0,0 +1,27 @@
+// This is this first program in the 20over20 series.
+// This program takes in an array and calculates the average of those numbers.
+// October 27th, 2023.
+// By Queenie Zhao.
+
+#include <iostream>
+
+using namespace std;
+
+main() {
+    // Create vector for storage.
+    
+    // Create variable to store average number.
+
+    // Message to prompt user to enter a list of numbers each separated by a space.
+
+    // Takes in numbers entered and store in vector dynamically.
+
+    // Checks if vector is empty. Ask user to input again if so.
+
+    // Adds all numbers in vectors and divide by size of vector.
+
+    // Outputs average number. Ask user if they want to input again.
+
+    // It's not much but it's honest work.
+    return 0;
+}
